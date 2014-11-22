c0d92bfd5efd78b05f11897d9c0ede79c88b5505
scanniza <simon@cymonit.se>
2014-11-22 19:43:40 +0100

    first anugukar logic

--- app/controllers/controllers.js
+++ app/controllers/controllers.js
@@ -1,5 +1,13 @@
 var app = angular.module('janu');
 
-app.controller('januController', ['$scope', function ($scope){
-    
+app.controller('januController', ['$scope','$http', function ($scope,$http){
+	var janu = this;
+	janu.timelineSongs = {};
+	$http.get('data/output.json').success(function(data){
+			janu.songs = data;
+			
+	});
+	$scope.addSong = function (evt){
+		console.log("blerf");
+	};
 }]);
