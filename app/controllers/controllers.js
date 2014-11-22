79353d69cca8a2564ae095071a296cfadffe32d8
scanniza <simon@cymonit.se>
2014-11-22 20:17:16 +0100

    minor changes

--- app/controllers/controllers.js
+++ app/controllers/controllers.js
@@ -2,12 +2,24 @@ var app = angular.module('janu');
 
 app.controller('januController', ['$scope','$http', function ($scope,$http){
 	var janu = this;
-	janu.timelineSongs = {};
+	janu.timelineSongs = [];
+	janu.timelineYears = [];
 	$http.get('data/output.json').success(function(data){
 			janu.songs = data;
 			
 	});
 	$scope.addSong = function (evt){
-		console.log("blerf");
+		var years = _.without(_.keys(janu.songs),janu.timelineYears);
+		var year = years[Math.floor(Math.random()*years.length)];
+		var randomSong = getRandomSong(janu.songs[year]);
+		randomSong.year = year;
+		janu.timelineSongs.push(randomSong);
+		janu.timelineYears.push(year);
+		console.log(janu.timelineYears);
 	};
+
 }]);
+
+function getRandomSong(year){
+	return year[Math.floor(Math.random()*year.length)];
+}
