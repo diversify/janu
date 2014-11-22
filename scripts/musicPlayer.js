51ac35594c3958079d7146053f938aac7baa90d4
scanniza <simon@cymonit.se>
2014-11-23 00:39:15 +0100

    time oer song option

--- scripts/musicPlayer.js
+++ scripts/musicPlayer.js
@@ -1,7 +1,14 @@
 var audio = new Audio();
-
+var TIME_PER_SONG = 15.0;
 function playSong(song){
 	audio.src = song.preview_url;
+	var skipped = false;
+	audio.addEventListener("canplay",function(){
+		if(!skipped){
+			audio.currentTime = 30.0-TIME_PER_SONG;
+			skipped=true;
+		}
+	});
 	audio.play();
 }
 
