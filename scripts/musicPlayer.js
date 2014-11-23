669f56ff8de0d9c53f2e68c739306656a7b19e7d
Yoav Zimmerman <yoavzimmerman@gmail.com>
2014-11-23 10:43:37 +0100

    dynamically load preview urls

--- scripts/musicPlayer.js
+++ scripts/musicPlayer.js
@@ -1,15 +1,27 @@
 var audio = new Audio();
 var TIME_PER_SONG = 15.0;
+
 function playSong(song){
-	audio.src = song.preview_url;
-	var skipped = false;
-	audio.addEventListener("canplay",function(){
-		if(!skipped){
-			audio.currentTime = 30.0-TIME_PER_SONG;
-			skipped=true;
-		}
-	});
-	audio.play();
+  console.log(song.song_id);
+  previewURL(song.song_id, function (preview_url) {
+    audio.src = preview_url;
+    console.log(preview_url);
+    var skipped = false;
+    audio.addEventListener("canplay",function(){
+      if(!skipped){
+        audio.currentTime = 30.0-TIME_PER_SONG;
+        skipped=true;
+      }
+    });
+    audio.play();
+  });
+}
+
+function previewURL(songId, callback) {
+  BASE_URL = "https://api.spotify.com/v1/tracks/";
+  $.get(BASE_URL + songId, function (data) {
+    callback(data.preview_url); 
+  });
 }
 
 function getTimeLeft(){
